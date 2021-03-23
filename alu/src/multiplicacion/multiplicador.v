6c01e524a76aeb7829ab18e8dcb364fae55abc17
machadod2000 <64340353+machadod2000@users.noreply.github.com>
2021-03-23 15:45:39 -0500

    Arreglos al modulo del multiplicador

--- alu/src/multiplicacion/multiplicador.v
+++ alu/src/multiplicacion/multiplicador.v
@@ -1,67 +1,57 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:40:46 09/13/2019 
-// Design Name: 
-// Module Name:    multiplicador 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module multiplicador( input [2:0] MR, 
-							 input [2:0] MD, 
-							input init, 
-							 input clk,  
-							 output reg [5:0] pp, 
+
+module multiplicador( input [2:0] MR,
+							input [2:0] MD,
+						   	input init,
+							 input clk,
+							 output  reg [5:0] res,
 							 output reg done
     );
+//always @(posedge clk) begin
+// if (init) begin
+// pp= MR*MD;
+// done=1;
+// end
+// end
+
 
 reg sh;
 reg rst;
 reg add;
 reg [5:0] A;
 reg [2:0] B;
+reg [5:0] pp;
 wire z;
 
 reg [2:0] status =0;
 
-// bloque comparador 
+// bloque comparador
 assign z=(B==0)?1:0;
 
 
 //bloques de registros de desplazamiento para A y B
 always @(posedge clk) begin
-   
+
 	if (rst) begin
 		A = {3'b000,MD};
 		B = MR;
 	end
-	else	begin 
+	else	begin
 		if (sh) begin
 			A= A << 1;
 			B = B >> 1;
 		end
 	end
 
-end 
+end
 
 //bloque de add pp
 always @(posedge clk) begin
-   
+
 	if (rst) begin
 		pp =0;
 	end
-	else	begin 
+	else	begin
 		if (add) begin
 		pp =pp+A;
 		end
@@ -69,7 +59,7 @@ always @(posedge clk) begin
 
 end
 
-// FSM 
+// FSM
 parameter START =0,  CHECK =1, ADD =2, SHIFT =3, END1 =4;
 
 always @(posedge clk) begin
@@ -77,13 +67,13 @@ always @(posedge clk) begin
 	START: begin
 		sh=0;
 		add=0;
-		if (init) begin
+		if (init==1) begin
 			status=CHECK;
 			done =0;
 			rst=1;
 		end
 		end
-	CHECK: begin 
+	CHECK: begin
 		done=0;
 		rst=0;
 		sh=0;
@@ -115,13 +105,14 @@ always @(posedge clk) begin
 		rst =0;
 		sh =0;
 		add =0;
+		res = pp[5:1];
 		status =START;
 	end
 	 default:
 		status =START;
-	endcase 
-	
-end 
+	endcase
+
+end
 
 
 endmodule
