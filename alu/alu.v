fcc72ad040412b9bb59c8a651e81d755dc244228
Andres Rodríguez Contreras <androdriguezco@unal.edu.co>
2021-03-23 15:55:51 -0500

    Se agrego el modulo de restador y a su vez se llamo en alu

--- alu/alu.v
+++ alu/alu.v
@@ -12,21 +12,21 @@ module alu(
  );
 
 	//Hola otra vez
-// Declaración de salidas de cada bloque 
+// Declaración de salidas de cada bloque
 wire [3:0] sal_suma;
 wire [3:0] sal_resta;
 wire [3:0] sal_div;
 wire [5:0] sal_mult;
 
 
-// Declaración de las entradas init de cada bloque 
-reg [3:0] init; 
+// Declaración de las entradas init de cada bloque
+reg [3:0] init;
 wire init_suma;
 wire init_resta;
 wire init_mult;
 wire init_div;
 
-// 
+//
 
 assign init_suma= init[0];
 assign init_resta=init[1];
@@ -39,7 +39,7 @@ wire [3:0] operacion;
 
 // descripción del decodificacion de operaciones
 always @(*) begin
-	case(opcode) 
+	case(opcode)
 		2'b00: init<=1;
 		2'b01: init<=2;
 		2'b10: init<=4;
@@ -47,11 +47,11 @@ always @(*) begin
 	default:
 		init <= 0;
 	endcase
-	
+
 end
 // Descripcion del miltiplexor
 always @(*) begin
-	case(opcode) 
+	case(opcode)
 		2'b00: int_bcd <={8'b00,sal_suma};
 		2'b01: int_bcd <={8'b00,sal_resta};
 		2'b10: int_bcd <={8'b00,sal_mult};
@@ -59,15 +59,16 @@ always @(*) begin
 	default:
 		int_bcd <= 0;
 	endcase
-	
+
 end
 
 
-//instanciación de los componnetes 
+//instanciación de los componnetes
 
 sum4b sum(. init(init_suma),.xi({1'b0,portA}), .yi({1'b0,portB}),.sal(sal_suma));
 multiplicador mul ( .MR(portA), .MD(portB), .init(init_mult),.clk(clk), .pp(sal_mult));
 display dp( .num(int_bcd), .clk(clk), .sseg(sseg), .an(an), .rst(rst));
+resta resta(. init(init_resta),.x({1'b0,portA}), .y({1'b0,portB}),.sal(sal_resta));
 
 // adicone los dos bloques que hacen flata la resta y división
 
